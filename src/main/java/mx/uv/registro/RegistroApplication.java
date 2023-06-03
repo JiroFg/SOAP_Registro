0ead18be06ee54069bccb3ef7d1ba204f6b66ece
Rousing <zs20018188@estudiantes.uv.mx>
2023-06-03 10:08:24 -0600

    cors

--- src/main/java/mx/uv/registro/RegistroApplication.java
+++ src/main/java/mx/uv/registro/RegistroApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @SpringBootApplication
-@CrossOrigin
 public class RegistroApplication {
 
 	public static void main(String[] args) {
