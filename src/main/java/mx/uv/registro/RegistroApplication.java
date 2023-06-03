b3c3adb0ab67e48c4cfbc523ba1fb18b4479632f
Rousing <zs20018188@estudiantes.uv.mx>
2023-06-03 10:22:11 -0600

    cors

--- src/main/java/mx/uv/registro/RegistroApplication.java
+++ src/main/java/mx/uv/registro/RegistroApplication.java
@@ -3,8 +3,9 @@ package mx.uv.registro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @SpringBootApplication
 public class RegistroApplication {
 
